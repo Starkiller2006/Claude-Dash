ea3f6bc0ce282cf1890e613c0fd6d866507ffd07
Starkiller2006 <43116182+Starkiller2006@users.noreply.github.com>
2026-05-21 19:06:10 -0400

    Create login.js

---
+++ pages/api/auth/login.js
@@ -0,0 +1,20 @@
+// pages/api/auth/login.js
+export default function handler(req, res) {
+  const params = new URLSearchParams({
+    client_id: process.env.WHOOP_CLIENT_ID,
+    redirect_uri: process.env.WHOOP_REDIRECT_URI,
+    response_type: "code",
+    scope: [
+      "offline",
+      "read:recovery",
+      "read:cycles",
+      "read:workout",
+      "read:sleep",
+      "read:profile",
+      "read:body_measurement",
+    ].join(" "),
+  });
+
+  const authURL = `https://api.prod.whoop.com/oauth/oauth2/auth?${params.toString()}`;
+  res.redirect(authURL);
+}
