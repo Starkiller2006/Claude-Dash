de80573dc687b5712e2b0a5e6561a740371e898e
Starkiller2006 <43116182+Starkiller2006@users.noreply.github.com>
2026-05-21 18:50:19 -0400

    Delete lifeos directory

--- lifeos/pages/api/auth/status.js
+++
@@ -1,7 +0,0 @@
-// pages/api/auth/status.js
-import { getSession } from "../../../lib/session";
-
-export default async function handler(req, res) {
-  const session = await getSession(req, res);
-  res.status(200).json({ authenticated: !!session.access_token });
-}
